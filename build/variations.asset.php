6a872ea9cb71ca255f2212d75cd6ad7b999ecfba
Carsten Bach <mail@carsten-bach.de>
2024-06-07 10:06:51 +0200

    BUILD

--- build/variations.asset.php
+++ build/variations.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-hooks', 'wp-i18n'), 'version' => 'b532464bee761ac2c09a');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-hooks', 'wp-i18n'), 'version' => 'dcd3e9b32a7f5291fe2b');
